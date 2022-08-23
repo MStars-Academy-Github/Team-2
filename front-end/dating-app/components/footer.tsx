0336ac981ed381a53733737b090041b2fe15e126
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-08-23 12:16:25 +0800

    ADD Bottom Navigation Icons and FavIcon

--- front-end/dating-app/components/footer.tsx
+++ front-end/dating-app/components/footer.tsx
@@ -2,15 +2,17 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import ClearIcon from "@mui/icons-material/Clear";
+import StarIcon from "@mui/icons-material/Star";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+import BoltIcon from "@mui/icons-material/Bolt";
 
 export default function Footer() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box sx={{ width: 500, marginBottom: "30vh" }}>
       <BottomNavigation
         showLabels
         value={value}
@@ -18,9 +20,26 @@ export default function Footer() {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction
+          label="Next One"
+          icon={<RefreshIcon color="warning" />}
+        />
+        <BottomNavigationAction
+          label="Clear from List"
+          icon={<ClearIcon color="error" />}
+        />
+        <BottomNavigationAction
+          label="Add Star"
+          icon={<StarIcon color="info" />}
+        />
+        <BottomNavigationAction
+          label="I like it!"
+          icon={<FavoriteIcon color="success" />}
+        />
+        <BottomNavigationAction
+          label="Speed"
+          icon={<BoltIcon color="primary" />}
+        />
       </BottomNavigation>
     </Box>
   );
