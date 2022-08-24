97010e10539a655dbcd1fc1a3cd7fe7ebdd406a4
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-08-24 13:28:53 +0800

    MOD main page

--- front-end/dating-app/components/navbar.tsx
+++ front-end/dating-app/components/navbar.tsx
@@ -4,8 +4,13 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 export default function Navbar() {
+  const [user, setUser] = React.useState<any>();
+  React.useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user") || "user"));
+  }, []);
   function logoutHandler(e: any) {
     e.preventDefault();
     window.localStorage.removeItem("user");
@@ -34,10 +39,29 @@ export default function Navbar() {
           >
             Dating App
           </Typography>
-
-          <Button color="inherit" onClick={logoutHandler}>
-            Logout
-          </Button>
+          <FormControl style={{ minWidth: "10vw" }} color="error">
+            <InputLabel id="labelForEmail" color="error">
+              {user?.data.email}
+            </InputLabel>
+            <Select
+              value={user?.data.email}
+              labelId="labelForEmail"
+              style={{ backgroundColor: "white" }}
+              variant="outlined"
+              color="error"
+            >
+              <MenuItem>
+                <Button color="success" href="/myprofile">
+                  Edit profile
+                </Button>
+              </MenuItem>
+              <MenuItem>
+                <Button color="error" onClick={logoutHandler}>
+                  Logout
+                </Button>
+              </MenuItem>
+            </Select>
+          </FormControl>
         </Toolbar>
       </AppBar>
     </Box>
