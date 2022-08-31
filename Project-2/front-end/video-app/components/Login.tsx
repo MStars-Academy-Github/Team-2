9703cb6dbb911ef6be963fec53c98b5805938ff7
ADD new file web <gerelrosegerel@gmai.com>
2022-09-01 00:31:07 +0800

    Add file

--- Project-2/front-end/video-app/components/Login.tsx
+++ Project-2/front-end/video-app/components/Login.tsx
@@ -1,7 +1,222 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, TextField } from "@mui/material";
+import axios from "axios";
 
 type Props = {};
 
 export default function Login({}: Props) {
-  return <div>Login</div>;
+  const [register, setRegister] = useState(false);
+  const [open, setOpen] = React.useState(false);
+
+  function loginHandler(e: any) {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:3001/v1/auth/login", {
+        email: e.target.email.value,
+        password: e.target.password.value,
+      })
+      .then((res) => {
+        if (res.status == 200) {
+          window.localStorage.setItem("user", JSON.stringify(res.data));
+          window.location.reload();
+        }
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+  }
+
+  function registerHandler(e: any) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/v1/users", {
+        email: e.target.email.value,
+        password: e.target.password.value,
+        firstName: e.target.firstName.value,
+        lastName: e.target.lastName.value,
+        phone: e.target.phone.value,
+        register: e.target.register.value,
+      })
+      .then((res) => {
+        if (res.status == 200) {
+          alert("amjilttai burtgelee");
+          setRegister(false);
+        }
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+  }
+
+  function registerHandlerOpen() {
+    setRegister(true);
+  }
+  return (
+    <div>
+      {register === false ? (
+        <Box
+          component="form"
+          sx={{
+            width: "100%",
+          }}
+          noValidate
+          autoComplete="off"
+          onSubmit={loginHandler}
+        >
+          <div>
+            <TextField
+              required
+              label="Email"
+              name="email"
+              variant="standard"
+              type="email"
+              style={{
+                width: "100%",
+                marginBottom: "20px",
+              }}
+            />
+
+            <TextField
+              id="standard-password-input"
+              name="password"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="standard"
+              style={{
+                width: "100%",
+                marginBottom: "20px",
+              }}
+            />
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            style={{
+              backgroundColor: "green",
+              width: "100%",
+              marginTop: "20px",
+              background: "#e63946",
+            }}
+          >
+            login
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            style={{
+              backgroundColor: "green",
+              width: "100%",
+              marginTop: "20px",
+              background: "#e63946",
+            }}
+            onClick={registerHandlerOpen}
+          >
+            register
+          </Button>
+        </Box>
+      ) : (
+        <Box
+          component="form"
+          sx={{
+            width: "100%",
+            padding: "20%",
+          }}
+          noValidate
+          autoComplete="off"
+          onSubmit={registerHandler}
+        >
+          <div>
+            <TextField
+              required
+              label="Email"
+              name="email"
+              variant="standard"
+              type="email"
+              style={{
+                width: "100%",
+                marginBottom: "20px",
+              }}
+            />
+            <TextField
+              required
+              label="firstName"
+              defaultValue="Hello World"
+              variant="standard"
+              name="firstName"
+              type="text"
+              style={{
+                width: "100%",
+                marginBottom: "20px",
+              }}
+            />
+            <TextField
+              required
+              label="lastName"
+              name="lastName"
+              defaultValue="Hello World"
+              variant="standard"
+              type="text"
+              style={{
+                width: "100%",
+                marginBottom: "20px",
+              }}
+            />
+            <TextField
+              required
+              label="phone"
+              name="phone"
+              defaultValue="Hello World"
+              variant="standard"
+              type="number"
+              style={{
+                width: "100%",
+                marginBottom: "20px",
+              }}
+            />
+            <TextField
+              required
+              label="register"
+              name="register"
+              defaultValue="Hello World"
+              variant="standard"
+              type="text"
+              style={{
+                width: "100%",
+                marginBottom: "20px",
+              }}
+            />
+            <TextField
+              label="password"
+              type="password"
+              name="password"
+              autoComplete="current-password"
+              variant="standard"
+              style={{
+                width: "100%",
+                marginBottom: "20px",
+              }}
+            />
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            style={{
+              backgroundColor: "green",
+              width: "100%",
+              marginTop: "20px",
+              background: "#e63946",
+            }}
+          >
+            register
+          </Button>
+        </Box>
+      )}
+    </div>
+  );
 }
