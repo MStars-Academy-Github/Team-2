bbb79dfa6b56933df972bb6e14c1c6e2b8cd9139
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-09-06 10:33:23 +0800

    Watch page

--- Project-2/front-end/video-app/pages/watch.tsx
+++ Project-2/front-end/video-app/pages/watch.tsx
@@ -1,11 +1,16 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 import ReactPlayer from "react-player";
 import { Button } from "@mui/material";
+import moment from "moment";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import ShareIcon from "@mui/icons-material/Share";
+import ContentCutIcon from "@mui/icons-material/ContentCut";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
 
 export default function watch({ videoDesc }: any) {
-  console.log(videoDesc);
   const router = useRouter();
   const { video } = router.query;
 
@@ -25,49 +30,104 @@ export default function watch({ videoDesc }: any) {
       return videos;
     }
   });
+  function handlerOpenVideo(id: any) {
+    router.push(`/watch?video=${id}`);
+  }
   console.log(watchVideo);
+
   return (
     <div
-      className="p-6"
-      style={{
-        display: "flex",
-        // justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        // backgroundColor: "#343a40",
-      }}
+      className="p-6 flex gap-4 justify-center "
+      style={{ backgroundColor: "#343a40", height: "100%" }}
     >
-      <ReactPlayer
-        url={`http://localhost:3001/v1/media/video/${video}`}
-        controls={true}
-        width="70%"
-        height="fit-content"
-      />
       <div>
-        <Button
-          sx={{
+        <ReactPlayer
+          url={`http://localhost:3001/v1/media/video/${video}`}
+          controls={true}
+          height="70% "
+          width="fit-content"
+        />
+        <div>
+          <p style={{ fontSize: "30px", color: "white" }}>
+            {watchVideo[0].title}
+          </p>
+          <div className="flex justify-between">
+            <div style={{ color: "white", display: "flex", gap: "5px" }}>
+              <p>Views {watchVideo[0].views}</p>
+              &#8226;
+              <p>{moment(watchVideo[0].created).format("MMM DD,YYYY")}</p>
+            </div>
+            <div>
+              <Button className="gap-2">
+                <ThumbUpIcon /> LIKE
+              </Button>
+              <Button className="gap-2">
+                <ThumbDownIcon />
+                DISLIKE
+              </Button>
+              <Button className="gap-2">
+                <ShareIcon />
+                SHARE
+              </Button>
+              <Button className="gap-2">
+                <ContentCutIcon />
+                CLIP
+              </Button>
+              <Button className="gap-2">
+                <WatchLaterIcon />
+                WATCH LATER
+              </Button>
+            </div>
+          </div>
+          <Button
+            sx={{
+              color: "white",
+              border: "0.2px solid #006c6e",
+              width: "200px",
+              margin: "10px",
+            }}
+            variant="outlined"
+            onClick={handlerEditVideo}
+          >
+            Edit
+          </Button>
+          <Button
+            sx={{
+              color: "white",
+              border: "0.2px solid #006c6e",
+              width: "200px",
+              margin: "10px",
+            }}
+            variant="outlined"
+            onClick={() => handlerDlt(`${video}`)}
+          >
+            Delete
+          </Button>
+        </div>
+      </div>
+      <div className="overflow-y-auto grid grid-cols-1 gap-5 h-[60vh]">
+        <div
+          style={{
+            backgroundColor: "#006c6e",
+            textAlign: "center",
             color: "white",
-            border: "0.2px solid #006c6e",
-            width: "200px",
-            margin: "10px",
+            padding: "10px",
           }}
-          variant="outlined"
-          onClick={handlerEditVideo}
         >
-          Edit
-        </Button>
-        <Button
-          sx={{
-            color: "white",
-            border: "0.2px solid #006c6e",
-            width: "200px",
-            margin: "10px",
-          }}
-          variant="outlined"
-          onClick={() => handlerDlt(`${video}`)}
-        >
-          Delete
-        </Button>
+          Related Videos
+        </div>
+        {videoDesc.map((video: any, i: number) => {
+          return (
+            <ReactPlayer
+              url={`http://localhost:3001/v1/media/video/${video._id}`}
+              controls={true}
+              width="100%"
+              height="fit-content"
+              onClick={() => handlerOpenVideo(video._id)}
+              key={i}
+            />
+          );
+        })}
       </div>
     </div>
   );
