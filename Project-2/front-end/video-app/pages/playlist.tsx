0e5d0371aa5c965aab9cba12c2fb548b9faac283
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-09-07 12:53:04 +0800

    Remove from playlist working

--- Project-2/front-end/video-app/pages/playlist.tsx
+++ Project-2/front-end/video-app/pages/playlist.tsx
@@ -1,24 +1,55 @@
+import { Avatar, Button } from "@mui/material";
 import axios from "axios";
+import moment from "moment";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 export default function Playlist() {
   const [user, setUser] = useState<any>();
+  const [playlist, setPlaylist] = useState<any>();
+  const [users, setUsers] = useState<any>([]);
   const [media, setMedia] = useState<any>();
+
+  const router = useRouter();
   useEffect(() => {
     setUser(JSON.parse(localStorage.getItem("user") || "user"));
+    axios.get("http://localhost:3001/v1/users").then((res) => {
+      setUsers(res.data.data);
+    });
+    axios.get("http://localhost:3001/v1/media").then((res) => {
+      setMedia(res.data.data);
+    });
   }, []);
   useEffect(() => {
     axios
       .get(`http://localhost:3001/v1/users/${user?.user._id}`)
       .then((res) => {
-        setMedia(res.data.data.playlist);
+        setPlaylist(res.data.data.playlist);
       });
   }, [user]);
 
+  function handlerOpenVideo(id: any) {
+    router.push(`/watch?video=${id}`);
+  }
+
+  function removeFromPlaylist(id: any) {
+    axios
+      .post("http://localhost:3001/v1/users/removeplaylist", {
+        userId: user.user._id,
+        mediaId: id,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          window.location.reload();
+        }
+      });
+  }
+
   return (
-    <div className="grid grid-cols-4">
-      {media?.map((video: any, i: number) => {
+    <div className="grid grid-cols-4 gap-5 p-[30px]">
+      {playlist?.map((video: any, i: number) => {
         return (
           <div key={i}>
             <ReactPlayer
@@ -26,7 +57,74 @@ export default function Playlist() {
               controls={true}
               width="20vw"
               height="20vh"
+              onClick={() => handlerOpenVideo(video)}
             />
+            <div className="text-white w-full">
+              <div className="flex items-start p-2 gap-2 w-full">
+                <div className="w-full">
+                  {media?.map((videoInfo: any) => {
+                    if (videoInfo._id === video) {
+                      {
+                        return users.map((user: any, i: number) => {
+                          if (user._id === videoInfo.postedBy) {
+                            return (
+                              <div className="flex justify-between items-center w-full">
+                                <div className="flex gap-3" key={i}>
+                                  <Avatar
+                                    alt="Remy Sharp"
+                                    style={{
+                                      backgroundColor:
+                                        "rgb(" +
+                                        Math.floor(Math.random() * 255) +
+                                        "," +
+                                        Math.floor(Math.random() * 255) +
+                                        "," +
+                                        Math.floor(Math.random() * 255) +
+                                        ")",
+                                    }}
+                                  >
+                                    {user.firstName.slice(0, 1)}
+                                  </Avatar>
+                                  <div>
+                                    <p style={{ fontSize: "18px" }}>
+                                      {videoInfo.title}
+                                    </p>
+
+                                    <p className="text-gray-400 text-[14px] flex items-center">
+                                      {user.firstName}
+
+                                      <CheckCircleIcon
+                                        className="text-[14px] ml-1"
+                                        color="info"
+                                      />
+                                    </p>
+                                    <p className="text-gray-400 text-[14px]">
+                                      Views {videoInfo.views} &#8226;{" "}
+                                      {moment(videoInfo.created).format(
+                                        "MMM DD,YYYY"
+                                      )}
+                                    </p>
+                                  </div>
+                                </div>
+                                <Button
+                                  variant="contained"
+                                  className="h-[50%] bg-red-500"
+                                  onClick={() => {
+                                    removeFromPlaylist(video);
+                                  }}
+                                >
+                                  Remove
+                                </Button>
+                              </div>
+                            );
+                          }
+                        });
+                      }
+                    }
+                  })}
+                </div>
+              </div>
+            </div>
           </div>
         );
       })}
