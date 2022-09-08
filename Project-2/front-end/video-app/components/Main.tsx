f1ecf18cfd8fff0eaf4986ae70376b9dfa71a536
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-09-08 09:54:10 +0800

    MOD

--- Project-2/front-end/video-app/components/Main.tsx
+++ Project-2/front-end/video-app/components/Main.tsx
@@ -116,8 +116,8 @@ export default function Main() {
                 <ReactPlayer
                   url={`http://localhost:3001/v1/media/video/${video._id}`}
                   controls={true}
-                  width="20vw"
-                  height="20vh"
+                  width="100%"
+                  height="25vh"
                   onClick={() => handlerOpenVideo(video._id)}
                 />
 
